17f128d1f82f650dc18970cc82e3a57547c41c8b
Syx <1196081136@qq.com>
2022-11-08 15:44:04 -0600

    commit framework of homepage

---
+++ app/src/main/java/edu/illinois/cs465/grocerygo/layout/fragment/OngoingFragment.java
@@ -0,0 +1,20 @@
+package edu.illinois.cs465.grocerygo.layout.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import edu.illinois.cs465.grocerygo.R;
+
+public class OngoingFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.ongoing_fragment, container, false);
+    }
+}
