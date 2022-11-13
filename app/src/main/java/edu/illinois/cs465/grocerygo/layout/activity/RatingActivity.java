981d460af38bcdc759471a9142d71483a74fcece
Syx <1196081136@qq.com>
2022-11-13 00:26:14 -0600

    commit style

--- app/src/main/java/edu/illinois/cs465/grocerygo/layout/activity/RatingActivity.java
+++ app/src/main/java/edu/illinois/cs465/grocerygo/layout/activity/RatingActivity.java
@@ -1,4 +1,18 @@
 package edu.illinois.cs465.grocerygo.layout.activity;
 
-public class RatingActivity {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.illinois.cs465.grocerygo.R;
+
+public class RatingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rating_layout);
+//        initView();
+    }
 }
