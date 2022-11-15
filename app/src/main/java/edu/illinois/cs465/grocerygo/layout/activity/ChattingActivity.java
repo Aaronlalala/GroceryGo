1e17c760535f41c60f7ad1798bda95736985af53
Zhicheng Tang <46953357+Aaronlalala@users.noreply.github.com>
2022-11-14 23:47:02 -0600

    Create chatting activity

---
+++ app/src/main/java/edu/illinois/cs465/grocerygo/layout/activity/ChattingActivity.java
@@ -0,0 +1,15 @@
+package edu.illinois.cs465.grocerygo.layout.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import edu.illinois.cs465.grocerygo.R;
+
+public class ChattingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatting);
+    }
+}
