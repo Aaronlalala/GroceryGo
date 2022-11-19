466007a5677fa2c4e7f40fb505e0593e6c2fa60b
Zhicheng Tang <46953357+Aaronlalala@users.noreply.github.com>
2022-11-18 23:46:30 -0600

    Revert "Merge branch 'main' into tzc"
    This reverts commit f70136b82a492ae390072a2b0434f66f3b343617, reversing
    changes made to d37137c066c1ca491ac76dd7136879336091421e.

--- app/src/main/java/edu/illinois/cs465/grocerygo/layout/activity/PostDetailActivity.java
+++ app/src/main/java/edu/illinois/cs465/grocerygo/layout/activity/PostDetailActivity.java
@@ -9,61 +9,24 @@ import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.MapsInitializer;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
 import edu.illinois.cs465.grocerygo.R;
 import edu.illinois.cs465.grocerygo.layout.fragment.OngoingFragment;
 
 public class PostDetailActivity extends AppCompatActivity {
-
-    private MapView mMap;
-    private GoogleMap googleMap;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.post_detail_activity);
-
-        mMap = findViewById(R.id.map);
-        mMap.onCreate(savedInstanceState);
-        mMap.onResume();
-        MapsInitializer.initialize(this);
-        int errorCode = GooglePlayServicesUtil
-                .isGooglePlayServicesAvailable(this);
-
-        if (ConnectionResult.SUCCESS != errorCode) {
-            GooglePlayServicesUtil.getErrorDialog(errorCode,
-                    this, 0).show();
-        } else {
-//            googleMap = mMap.getMap();
-            if (googleMap != null) {
-//                initVolley();
-                LatLng cur = new LatLng(40.116890, -88.222130);
-                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(cur, 15));
-                googleMap.addMarker(new MarkerOptions().position(cur));
-            }
-        }
-
         initView();
     }
 
     private void initView() {
         ImageView postView = findViewById(R.id.back);
-
-
-
         postView.setOnClickListener(view -> {
             Intent intent = new Intent(this, MainActivity.class);
             this.startActivity(intent);
@@ -82,4 +45,5 @@ public class PostDetailActivity extends AppCompatActivity {
         ft.add(R.id.post_detail_container, fragment);
         ft.commit();
     }
+
 }
